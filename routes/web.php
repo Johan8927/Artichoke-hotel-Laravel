cd2919a572529ccc25d64332968fc6fffcf6791e
Johan <johan.bertrand@le-campus-numerique.fr>
2025-01-20 15:14:19 +0100

    Auth OK / LandingPageHero OK / migration+factory+seeder all OK

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,23 @@ use App\Http\Controllers\LandingPageHeroController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/landingPageHero', [LandingPageHeroController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+// Auth routes
 require __DIR__ . '/auth.php';
 
-
-
+// Ressources des contrôleurs
+Route::resource('landingpageheroes', LandingPageHeroController::class);
 Route::resource('landingpagenews', 'LandingPageNewsController');
-
 Route::resource('landingpageamenities', 'LandingPageAmenitiesController');
-
-Route::resource('landingpageheroes', 'LandingPageHeroController');
-Route::get('/landingPageHero', [LandingPageHeroController::class, 'index']);
-
 Route::resource('hotelroomtypes', 'HotelRoomTypesController');
